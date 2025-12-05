43c233cf75e8ba63f7b0d26f0a270cfecaf829bd
simonredfern <simon@tesobe.com>
2025-12-06 00:44:01 +0100

    Clean up left bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -671,39 +671,6 @@
                     <span>{subItem.label}</span>
                   </a>
                 {/each}
-
-                {#if systemDynamicEntities.length > 0}
-                  <div
-                    class="my-2 border-t border-gray-300 dark:border-gray-600"
-                  ></div>
-                  {#each systemDynamicEntities as entity}
-                    {@const entityName =
-                      Object.keys(entity).find(
-                        (key) =>
-                          ![
-                            "userId",
-                            "dynamicEntityId",
-                            "hasPersonalEntity",
-                          ].includes(key),
-                      ) || "Unknown"}
-                    <a
-                      href="/dynamic-entities/system/{entity.dynamicEntityId}/crud"
-                      class="btn w-full justify-start gap-3 px-2 pl-6 text-sm hover:preset-tonal"
-                      class:preset-filled-secondary-50-950={page.url
-                        .pathname ===
-                        `/dynamic-entities/system/${entity.dynamicEntityId}/crud`}
-                      class:border-l-2={page.url.pathname ===
-                        `/dynamic-entities/system/${entity.dynamicEntityId}/crud`}
-                      class:border-primary-500={page.url.pathname ===
-                        `/dynamic-entities/system/${entity.dynamicEntityId}/crud`}
-                      title={entityName}
-                      aria-label={entityName}
-                    >
-                      <Database class="size-4" />
-                      <span>{entityName}</span>
-                    </a>
-                  {/each}
-                {/if}
               </Navigation.Menu>
             {/if}
           </Navigation.Group>
